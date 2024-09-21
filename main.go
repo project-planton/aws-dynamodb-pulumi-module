18db66ff5d873af7cf86db222259b40204419029
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-21 16:20:41 +0530

    updated apis to be fetched from the new cli project location

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/pkg/errors"
 	"github.com/plantoncloud/aws-dynamodb-pulumi-module/pkg"
-	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/awsdynamodb"
+	"github.com/plantoncloud/planton/apis/zzgo/cloud/planton/apis/code2cloud/v1/aws/awsdynamodb"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
